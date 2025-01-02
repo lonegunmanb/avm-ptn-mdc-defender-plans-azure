395088f3f5485ed6ae93f82a55cc6c55073f3887
hezijie <lonegunmanb@hotmail.com>
2025-01-02 17:03:18 +0800

    debug

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "3.6.2"
-    }
-  }
-}
 # module "mdc_plans_enable" {
 #   source           = "../.."
 #   location         = "westeurope"
