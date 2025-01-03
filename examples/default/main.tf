68f2e1e890f0a83bef086edc5535634abd11f607
hezijie <lonegunmanb@hotmail.com>
2025-01-03 10:54:46 +0800

    have setup and teardown, but test failed

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,7 +1,7 @@
-module "mdc_plans_enable" {
-  source           = "../.."
-  location         = "westeurope"
-  mdc_plans_list   = var.mdc_plans_list
-  subplans         = var.subplans
-  enable_telemetry = var.enable_telemetry
-}
+# module "mdc_plans_enable" {
+#   source           = "../.."
+#   location         = "westeurope"
+#   mdc_plans_list   = var.mdc_plans_list
+#   subplans         = var.subplans
+#   enable_telemetry = var.enable_telemetry
+# }
